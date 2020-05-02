f15d53fe2e071172eee0e47d7b7241cf36f42263
mossax <ibuki7720@yahoo.co.jp>
2020-05-02 12:51:48 +0900

    qandaページを追加した

---
+++ page-qanda.php
@@ -0,0 +1,49 @@
+<?php get_header(); ?>
+<div class="hero__section qanda__hero-img">
+  <h1 class="page-title">
+    Q&A
+  </h1>
+</div>
+<div class="works-single__wrapper" style="margin-bottom: 100px;">
+  <div class="works-single__heading__flex-box">
+    <div class="works-single__heading__title text">
+      <h2>
+        Q. 兼部・兼サーはできますか？
+      </h2>
+    </div>
+  </div>
+  <p class="works-single__heading__date">
+    A. できます！自分のペースで参加できます。バイトももちろんみんなしてます。ああああ
+  </p>
+  <div class="works-single__heading__flex-box" style="margin-top: 50px;">
+    <div class="works-single__heading__title text">
+      <h2>
+        Q. クリエティブな活動ができる気がしないのですが、入っても大丈夫ですか？
+      </h2>
+    </div>
+  </div>
+  <p class="works-single__heading__date">
+    A. みはんけんでは、全員がクリエイティブ活動をしているわけではありません。その他にも企画、FW参加や営業、取材活動などたくさんやることがあります！
+  </p>
+  <div class="works-single__heading__flex-box" style="margin-top: 50px;">
+    <div class="works-single__heading__title text">
+      <h2>
+        Q. 夏合宿は、具体的にいつですか？
+      </h2>
+    </div>
+  </div>
+  <p class="works-single__heading__date">
+    A. ああああ
+  </p>
+  <div class="works-single__heading__flex-box" style="margin-top: 50px;">
+    <div class="works-single__heading__title text">
+      <h2>
+        Q. FWやイベントの日程はどれくらい前に決まりますか？
+      </h2>
+    </div>
+  </div>
+  <p class="works-single__heading__date">
+    A. ああああ
+  </p>
+</div>
+<?php get_footer(); ?>
